50092fe6a3b30b3d28e6dfe52e27f8ff8ff57b69
omlnaut <oneironaut.oml@gmail.com>
2021-01-30 10:38:25 +0100

    finished chapter 3

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Following: https://www.youtube.com/watch?v=WQeoO7MI0Bs
 29/01/2020
 - Github setup
 - Installation
+- Chapter 1, loading
+- Chapter 2, basic functions
+
+30/01/2021
+- Chapter 3, Crop and Resize
---
+++ project/chapter3.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('resources/lambo.png')
+print(img.shape)
+
+resized = cv2.resize(img, (300,200))
+cropped = img[:200, 200:500]
+
+cv2.imshow('Image', img)
+cv2.imshow('Resized', resized)
+cv2.imshow('Cropped', cropped)
+cv2.waitKey(0)
